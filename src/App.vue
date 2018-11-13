0544de644ef48c7a0fdf8aa9d632ef75d7d5f45b
Blessing Orazulume <orazulumeblessing@gmail.com>
2018-11-13 22:54:20 +0100

    Parent component styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
+    <app-content></app-content>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Content from './components/SignUp.vue';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+      'app-header': Header,
+      'app-footer': Footer,
+      'app-content': Content
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: #F4F4F4;
+  }
+  #app {
+    width: 50%;
+    height: 100vh;
+    margin: 0 auto;
+    background: white;
+  }
 </style>
